11d42c659e943557a82d391ee39c956ce7209eb4
khkh@microsoft.com <khkh@microsoft.com>
2024-06-21 19:26:19 -0500

    increase the test timeout

--- test/Azure.Functions.Cli.Tests/E2E/InitTests.cs
+++ test/Azure.Functions.Cli.Tests/E2E/InitTests.cs
@@ -155,7 +155,7 @@ namespace Azure.Functions.Cli.Tests.E2E
                     $".vscode{Path.DirectorySeparatorChar}extensions.json",
                 },
                 OutputDoesntContain = new[] { "Initialized empty Git repository" },
-                CommandTimeout = TimeSpan.FromSeconds(120)
+                CommandTimeout = TimeSpan.FromSeconds(300)
             }, _output);
         }
 
@@ -298,7 +298,7 @@ namespace Azure.Functions.Cli.Tests.E2E
                         ContentContains = new[] { $"FROM mcr.microsoft.com/azure-functions/{workerRuntime}:{version}" }
                     }
                 },
-                CommandTimeout = TimeSpan.FromSeconds(120),
+                CommandTimeout = TimeSpan.FromSeconds(300),
                 OutputContains = new[] { "Dockerfile" }
             }, _output);
         }
@@ -480,7 +480,7 @@ namespace Azure.Functions.Cli.Tests.E2E
                     "Writing Dockerfile",
                     "Writing .dockerignore"
                 },
-                CommandTimeout = TimeSpan.FromSeconds(120)
+                CommandTimeout = TimeSpan.FromSeconds(300)
             }, _output);
         }
 
@@ -510,7 +510,7 @@ namespace Azure.Functions.Cli.Tests.E2E
                     "Writing local.settings.json",
                     $".vscode{Path.DirectorySeparatorChar}extensions.json",
                 },
-                CommandTimeout = TimeSpan.FromSeconds(120)
+                CommandTimeout = TimeSpan.FromSeconds(300)
             }, _output);
         }
 
@@ -581,7 +581,7 @@ namespace Azure.Functions.Cli.Tests.E2E
                     }
                 },
                 OutputContains = new[] { "Dockerfile" },
-                CommandTimeout = TimeSpan.FromSeconds(120)
+                CommandTimeout = TimeSpan.FromSeconds(300)
             }, _output);
         }
 
@@ -714,7 +714,7 @@ namespace Azure.Functions.Cli.Tests.E2E
                 {
                     "Writing host.json"
                 },
-                CommandTimeout = TimeSpan.FromSeconds(120)
+                CommandTimeout = TimeSpan.FromSeconds(300)
             }, _output);
         }
 
